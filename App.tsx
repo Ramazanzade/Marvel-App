4d2344d4812c3ed5e96121134ef7de70961d0136
Nizam <ramazanov570633@gmail.com>
2023-05-05 00:06:43 +0400

    Marvel

--- App.tsx
+++ App.tsx
@@ -21,7 +21,7 @@ const App = () => {
         <Stack.Screen name="PlansScreen" component={PlansScreen}/> */}
         {/* <Stack.Screen name="ProfileScreen" component={ProfileScreen}/>
         // <Stack.Screen name="HomeScreen" component={HomeScreen}/> */}
-        {/* <Stack.Screen name="Tabbar" component={Tabbar}/> */}
+        <Stack.Screen name="Tabbar" component={Tabbar}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
