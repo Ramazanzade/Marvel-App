6e729904aec8d1105385cfb44b0d060166e58c7d
Nizam <ramazanov570633@gmail.com>
2023-05-17 00:24:48 +0400

    Marvel12

--- src/Download/Download2.tsx
+++ src/Download/Download2.tsx
@@ -1,12 +1,53 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
+import downloadcss from './downloadcss';
 
-const Download2 = () => {
-  return (
-    <View>
-      <Text>Download2</Text>
-    </View>
-  )
+const Download2 = ({ navigation }: any) => {
+    const DATA = [
+        { id: 0, img: { uri: 'https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkwNTgxMjkxNjk3NDQ4ODI4/marveldisney.jpg' }, text: 'Spider-Man: Homecoming', strong: '1.2GB' },
+        { id: 1, img: { uri: 'https://www.lifehacker.com.au/wp-content/uploads/sites/4/2022/11/09/marvel-movies-watch-runtime.jpg?quality=80&resize=1280,720' }, text: 'Spider-Man: Homecoming', strong: '1.2GB' },
+        { id: 2, img: { uri: 'https://i.guim.co.uk/img/media/978bb76aaf05d05513f35f26c73ea61ad28b7f60/0_614_2628_1577/master/2628.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=0617fd285bbed119999eafa2ffb03e55' }, text: 'Spider-Man: Homecoming', strong: '1.2GB' }
+    ];
+    const render1 = ({ item }: any) => {
+        return (
+            <View style={downloadcss.imput}>
+                <View style={downloadcss.view}>
+                    <TouchableOpacity style={downloadcss.movieimgview} >
+                        <Image
+                            style={downloadcss.movieimg}
+                            source={item.img}
+                        />
+                    </TouchableOpacity>
+                    <View style={downloadcss.view3}>
+                        <Text style={downloadcss.text1}>{item.text}</Text>
+
+                        <View style={downloadcss.view1}>
+                       <View>
+                       <View style={downloadcss.view2}></View>
+                            <View style={downloadcss.view4}>
+                                <Text style={downloadcss.text2}>0% Watched</Text>
+                            </View>
+                       </View>
+                       <View>
+                        <Text style={downloadcss.text3}>{item.strong}</Text>
+                       </View>
+                        </View>
+                    </View>
+
+                </View>
+            </View>
+        )
+    }
+    return (
+        <View style={{ backgroundColor: 'black', flex: 1 }}>
+            <Text>Download2</Text>
+            <FlatList
+                data={DATA}
+                renderItem={render1}
+                contentContainerStyle={{ flexGrow: 1 }}
+            />
+        </View>
+    )
 }
 
 export default Download2
--- src/Download/Downloadcss.tsx
+++ src/Download/Downloadcss.tsx
@@ -1,13 +1,62 @@
 import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
-    imgview:{
-alignSelf:'center',
-marginTop:'-70%',
-marginRight:'20%',
-marginBottom:'-50%'
+    imgview: {
+        alignSelf: 'center',
+        marginTop: '-70%',
+        marginRight: '20%',
+        marginBottom: '-50%'
     },
-    img:{
-width:160
+    img: {
+        width: 160
     },
+    movieimgview: {
+
+    },
+    movieimg: {
+        width: 80,
+        height: 120,
+        marginLeft: '5%'
+    },
+    imput: {
+        backgroundColor: 'rgba(217, 217, 217, 0.1)',
+        width: '90%',
+        marginLeft: '5%',
+        height: 120,
+        marginBottom: '5%'
+    },
+    view: {
+        flexDirection: 'row',
+    },
+    text1:{
+        color:'#FFFFFF',
+        fontSize:15,
+        marginTop:'3%'
+    },
+    text2:{
+color:'#FFFFFF',
+fontSize:12
+    },
+    view1:{
+marginTop:'7%',
+flexDirection:'row',
+justifyContent:'space-between'
+    },
+    view2:{
+backgroundColor:'red',
+width:'80%',
+height:5,
+borderRadius:50,
+    },
+    view3:{
+        flexDirection:'column',
+        marginLeft:'2%'
+    },
+    view4:{
+        margin:'5%'
+    },
+    text3:{
+        color:'#FFFFFF'
+    }
+
 
 })
